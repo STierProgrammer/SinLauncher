62c930579a6c4b6716c6b7ddfd2276c542f8658c
STierProgrammer <150156387+STierProgrammer@users.noreply.github.com>
2024-07-05 23:23:36 +0400

    Check

--- src/main/java/com/sinlauncher/app/GUI/MainLauncherMenu.java
+++ src/main/java/com/sinlauncher/app/GUI/MainLauncherMenu.java
@@ -1,4 +1,28 @@
 package com.sinlauncher.app.GUI;
 
-public class MainLauncherMenu {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MainLauncherMenu extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Button btn = new Button();
+        
+        btn.setText("Hello JavaFX");
+        btn.setOnAction(event -> System.out.println("Hello JavaFX"));
+
+        Scene scene = new Scene(btn, 300, 250);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("JavaFX");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
+// Here
