7dceb4438bac8787046d0c804e914419e924bd03
Roxve <116169309+Roxve@users.noreply.github.com>
2024-07-05 21:31:13 +0300

    Launcher dir and maven change my-app

--- src/main/java/com/sinlauncher/app/App.java
+++ src/main/java/com/sinlauncher/app/App.java
@@ -1,12 +1,20 @@
 package com.sinlauncher.app;
 
-/**
- * Hello world!
- *
- */
 public class App {
+    private static final String LAUNCHER_DIR;
+
+    static {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")) {
+            LAUNCHER_DIR = System.getenv("APPDATA") + "\\SinLauncher";
+        } else if (os.contains("nix") || os.contains("nux") || os.contains("aix")) {
+            LAUNCHER_DIR = System.getProperty("user.home") + "/.sinlauncher";
+        } else {
+            LAUNCHER_DIR = "SinLauncher";
+        }
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Launcher Directory: " + LAUNCHER_DIR);
     }
 }
