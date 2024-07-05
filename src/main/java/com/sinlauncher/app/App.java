b4dafed3646c3827c42252e26bcb9351f9ed8a1f
Roxve <116169309+Roxve@users.noreply.github.com>
2024-07-05 21:55:48 +0300

    unirest and init_launcher_dir()

--- src/main/java/com/sinlauncher/app/App.java
+++ src/main/java/com/sinlauncher/app/App.java
@@ -1,5 +1,13 @@
 package com.sinlauncher.app;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import kong.unirest.core.HttpResponse;
+import kong.unirest.core.Unirest;
+
 public class App {
     private static final String LAUNCHER_DIR;
 
@@ -14,7 +22,44 @@ public class App {
         }
     }
 
+    static void init_launcher_dir() throws IOException {
+        if (!Files.exists(Paths.get(LAUNCHER_DIR))) {
+            Files.createDirectories(Paths.get(LAUNCHER_DIR));
+        }
+
+        if (!Files.exists(Paths.get(LAUNCHER_DIR + "/assets"))) {
+            Files.createDirectories(Paths.get(LAUNCHER_DIR + "/assets"));
+        }
+
+        if (!Files.exists(Paths.get(LAUNCHER_DIR + "/libraries"))) {
+            Files.createDirectories(Paths.get(LAUNCHER_DIR + "/libraries"));
+        }
+
+        if (!Files.exists(Paths.get(LAUNCHER_DIR + "/instances"))) {
+            Files.createDirectories(Paths.get(LAUNCHER_DIR + "/instances"));
+        }
+
+        // fetching manifest json or using an already downloaded one
+        HttpResponse<String> response = Unirest.get("https://launchermeta.mojang.com/mc/game/version_manifest.json").asString();
+        
+        Path path = Paths.get(LAUNCHER_DIR + "/version_manifest.json");
+
+        if (response.getStatus() == 200) {
+            Files.write(path, response.getBody().getBytes());
+        } else {
+            if (!Files.exists(path)) {
+                throw new IOException("Failed to fetch manifest JSON. Response code: " + response.getStatus());
+            }
+        }
+    }
+
     public static void main(String[] args) {
+        try {
+            init_launcher_dir();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("Launcher Directory: " + LAUNCHER_DIR);
     }
 }
