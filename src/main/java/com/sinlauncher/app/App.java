0f4a71fc57a975e485b292dfe16864f90f181ef6
STierProgrammer <150156387+STierProgrammer@users.noreply.github.com>
2024-07-06 14:30:48 +0400

    Keeping the code clean

--- src/main/java/com/sinlauncher/app/App.java
+++ src/main/java/com/sinlauncher/app/App.java
@@ -13,44 +13,43 @@ public class App {
 
     static {
         String os = System.getProperty("os.name").toLowerCase();
-        if (os.contains("win")) {
+        
+        if (os.contains("win")) 
             LAUNCHER_DIR = System.getenv("APPDATA") + "\\SinLauncher";
-        } else if (os.contains("nix") || os.contains("nux") || os.contains("aix")) {
+        
+        else if (os.contains("nix") || os.contains("nux") || os.contains("aix")) 
             LAUNCHER_DIR = System.getProperty("user.home") + "/.sinlauncher";
-        } else {
-            LAUNCHER_DIR = "SinLauncher";
-        }
+        
+        else 
+            LAUNCHER_DIR = "SinLauncher";     
     }
 
     static void init_launcher_dir() throws IOException {
-        if (!Files.exists(Paths.get(LAUNCHER_DIR))) {
+        if (!Files.exists(Paths.get(LAUNCHER_DIR))) 
             Files.createDirectories(Paths.get(LAUNCHER_DIR));
-        }
-
-        if (!Files.exists(Paths.get(LAUNCHER_DIR + "/assets"))) {
+        
+        if (!Files.exists(Paths.get(LAUNCHER_DIR + "/assets"))) 
             Files.createDirectories(Paths.get(LAUNCHER_DIR + "/assets"));
-        }
+        
 
-        if (!Files.exists(Paths.get(LAUNCHER_DIR + "/libraries"))) {
+        if (!Files.exists(Paths.get(LAUNCHER_DIR + "/libraries"))) 
             Files.createDirectories(Paths.get(LAUNCHER_DIR + "/libraries"));
-        }
-
-        if (!Files.exists(Paths.get(LAUNCHER_DIR + "/instances"))) {
+        
+        if (!Files.exists(Paths.get(LAUNCHER_DIR + "/instances"))) 
             Files.createDirectories(Paths.get(LAUNCHER_DIR + "/instances"));
-        }
+        
 
-        // fetching manifest json or using an already downloaded one
         HttpResponse<String> response = Unirest.get("https://launchermeta.mojang.com/mc/game/version_manifest.json").asString();
         
         Path path = Paths.get(LAUNCHER_DIR + "/version_manifest.json");
 
-        if (response.getStatus() == 200) {
+        if (response.getStatus() == 200) 
             Files.write(path, response.getBody().getBytes());
-        } else {
-            if (!Files.exists(path)) {
-                throw new IOException("Failed to fetch manifest JSON. Response code: " + response.getStatus());
-            }
+        else {
+            if (!Files.exists(path)) 
+                throw new IOException("Failed to fetch manifest JSON. Response code: " + response.getStatus());       
         }
+
     }
 
     public static void main(String[] args) {
