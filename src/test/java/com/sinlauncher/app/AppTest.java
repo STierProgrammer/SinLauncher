cf7d19d707fc79689deb67abab538da864973e0d
Anmol Singh <anmol.singh232004@gmail.com>
2024-07-08 19:05:52 +0530

    Encryptor added, Java Fx bugs Check MainLauncherMenu line 32-34

--- src/test/java/com/sinlauncher/app/AppTest.java
+++ src/test/java/com/sinlauncher/app/AppTest.java
@@ -1,7 +1,12 @@
 package com.sinlauncher.app;
 
 import static org.junit.Assert.assertTrue;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.junit.Test;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Unit test for simple com.App.
@@ -15,5 +20,25 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+@Test
+    public void testEncryptionKey() {
+            PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+            SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+            config.setPassword(System.getenv("ENCRYPTION_PASSWORD"));
+            config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
+            config.setKeyObtentionIterations("1000");
+            config.setPoolSize(1);
+            config.setProviderName("SunJCE");
+            config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+            config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+            config.setStringOutputType("base64");
+            encryptor.setConfig(config);
+
+            // FOR TESTING PURPOSE WILL REMOVE THIS LATER
+            // String plainText = "???";
+//            System.out.println("Encrypted value is : " + encryptor.encrypt(plainText));
+//            System.out.println(encryptor.decrypt("vXpoQcbIV1xysEBikQQLUgj3sTQnQX3QdnRiszkimtpMM6C/DFDlrhpi3qqj3wF6"));
     }
 }
