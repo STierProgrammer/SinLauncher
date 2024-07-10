42e05960aa29d5534a47f61daa33a511c68448fc
Anmol Singh <anmol.singh232004@gmail.com>
2024-07-10 16:27:47 +0530

    Added authentication and authorization, Added uuid mechanism for cracked and legit, set up jpa and modified user

--- src/main/java/com/example/SinLauncher/SinLauncherClasses/User.java
+++ src/main/java/com/example/SinLauncher/SinLauncherClasses/User.java
@@ -1,18 +1,48 @@
 package com.example.SinLauncher.SinLauncherClasses;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Setter
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    private String uuid;
+
     private String username;
     private String password;
     private String email;
     private boolean isLoggedIn;
+<<<<<<< HEAD
+=======
+
+    //Directly pass in Minecraft UUID
+    public User(UUID minecraftUUID, String username, String password, String email, boolean isLoggedIn) {
+        this.uuid = minecraftUUID.toString();
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.isLoggedIn = isLoggedIn;
+    }
+
+    //pass in generatedUUID here in String format
+    //UUID.randomUUID() generates a unique UUID
+    public User(String generatedUUID, String username, String password, String email, boolean isLoggedIn) {
+        this.uuid = generatedUUID + "--Sin";
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.isLoggedIn = isLoggedIn;
+    }
+>>>>>>> 6ddee7d (Added authentication and authorization, Added uuid mechanism for cracked and legit, set up jpa and modified user)
 }
