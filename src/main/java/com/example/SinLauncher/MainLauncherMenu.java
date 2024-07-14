97fb75cac7bc5d938ad2cdfd4f477be36f2e6788
STierProgrammer <150156387+STierProgrammer@users.noreply.github.com>
2024-07-14 20:50:27 +0400

    fixed merge errors

--- src/main/java/com/example/SinLauncher/MainLauncherMenu.java
+++ src/main/java/com/example/SinLauncher/MainLauncherMenu.java
@@ -9,7 +9,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-<<<<<<< HEAD
+
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -17,12 +17,10 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-=======
+
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
->>>>>>> 4a787a3e74cd80e9f0f9aedfc60845c057e093a0
 
 @SpringBootApplication
 @EnableEncryptableProperties
@@ -76,16 +74,13 @@ public class MainLauncherMenu extends Application {
         mainPane.setCenter(centerScrollPane);
         mainPane.setBottom(bottomSection);
 
-<<<<<<< HEAD
+
         Scene scene = new Scene(mainPane, 1200, 600);
         scene.getStylesheets().add(getClass().getResource("./styles/styles.css").toExternalForm());
 
         Image icon = new Image(getClass().getResourceAsStream("/Minecraft.png"));
         primaryStage.getIcons().add(icon);
-=======
-        //scene.getStylesheets().add(getClass().getResource("./styles/styles.css").toExternalForm()); // Load CSS file
 
->>>>>>> 4a787a3e74cd80e9f0f9aedfc60845c057e093a0
         primaryStage.setScene(scene);
         primaryStage.setTitle("com/example/SinLauncher");
         primaryStage.show();
@@ -102,9 +97,6 @@ public class MainLauncherMenu extends Application {
 
         launch(args);
         System.out.println("JavaFx Running");
-<<<<<<< HEAD
-=======
 
->>>>>>> 4a787a3e74cd80e9f0f9aedfc60845c057e093a0
     }
 }
--- src/main/java/com/example/SinLauncher/SinLauncherEntites/User.java
+++ src/main/java/com/example/SinLauncher/SinLauncherEntites/User.java
@@ -1,14 +1,14 @@
 package com.example.SinLauncher.SinLauncherEntites;
 
-<<<<<<< HEAD:src/main/java/com/example/SinLauncher/SinLauncherClasses/User.java
+
 import java.util.UUID;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-=======
+
 import jakarta.persistence.*;
->>>>>>> 4a787a3e74cd80e9f0f9aedfc60845c057e093a0:src/main/java/com/example/SinLauncher/SinLauncherEntites/User.java
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
