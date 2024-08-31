74163ce60298d0858d4257e615991b159fab6302
STierProgrammer <150156387+STierProgrammer@users.noreply.github.com>
2024-08-31 10:32:45 +0400

    Removed

--- src/main/resources/js/profile.js
+++ src/main/resources/js/profile.js
@@ -1,11 +1,1 @@
 // profile.js
-
-document.addEventListener("DOMContentLoaded", function () {
-    const username = window.javaApp.getCurrentUser(); 
-    
-    if (username) {
-        document.getElementById("username").textContent = username;
-    } else {
-        document.getElementById("username").textContent = "Unknown User";
-    }
-});
